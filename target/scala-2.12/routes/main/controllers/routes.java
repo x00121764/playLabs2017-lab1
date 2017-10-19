b8024d0285d595a77a243ce2347c5b63d57fdf83
Enda Lee <enda.lee@it-tallaght.ie>
2017-10-19 12:15:41 +0100

    first commit

---
+++ target/scala-2.12/routes/main/controllers/routes.java
@@ -0,0 +1,21 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/home/wdd/webapps/Playapps/HelloWorld/conf/routes
+// @DATE:Thu Oct 19 11:37:07 IST 2017
+
+package controllers;
+
+import router.RoutesPrefix;
+
+public class routes {
+  
+  public static final controllers.ReverseHomeController HomeController = new controllers.ReverseHomeController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets(RoutesPrefix.byNamePrefix());
+
+  public static class javascript {
+    
+    public static final controllers.javascript.ReverseHomeController HomeController = new controllers.javascript.ReverseHomeController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets(RoutesPrefix.byNamePrefix());
+  }
+
+}
